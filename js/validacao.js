e96a0c02d279a0c6bad44c01a5d8fd1a73fcdbbd
lucas <lucashtw@gmail.com>
2022-05-20 09:33:48 -0300

    testando liveserver

--- js/validacao.js
+++ js/validacao.js
@@ -1,32 +1,29 @@
-export function valida(input){
-  const tipoDeIput = input.dataset.tipo
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
 
-  if(validadores[tipoDeIput]){
-    validadores[tipoDeIput](input)
-  }
-
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
 }
 
 const validadores = {
-  dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input)
 }
 
-function validaDataNascimento(input){
+function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
     let mensagem = ''
-    
-    if(!maiorQue18(dataRecebida)){
-        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
 
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
     }
 
     input.setCustomValidity(mensagem)
 }
 
-function maiorQue18(data){
+function maiorQue18(data) {
     const dataAtual = new Date()
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(),data.getUTCDATE())
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
     return dataMais18 <= dataAtual
-
 }
