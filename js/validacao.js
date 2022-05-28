ac7357e0d8c72c83c2edd5720de5b1b3b75d2bce
lucas <lucashtw@gmail.com>
2022-05-28 10:32:39 -0300

    Validando CEP com regex

--- js/validacao.js
+++ js/validacao.js
@@ -40,7 +40,12 @@ const mensagensDeErro = {
     cpf:{
         valueMissing: 'O campo de CPF não pode estar vazio',
         customError: 'O CPF digitado não é valido'
+    },
+    cep: {
+        valueMissing: 'O campo de CEP não pode esta vazio',
+        patternMismatch: 'O CEP digitado não é valido'
     }
+
 
 }
 
